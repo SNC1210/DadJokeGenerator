e8b228584fd809b501170fe1ee3aacee3788e3dc
SNC1210 <47062459+SNC1210@users.noreply.github.com>
2021-07-02 15:34:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const jokeEle =document.getElementById('joke');
+const get_joke= document.getElementById('get_joke');
+
+get_joke.addEventListener('click',generateJoke);
+
+generateJoke();
+async function generateJoke(){
+    //call the icanhaz API
+    const jokeRes= await fetch('https://icanhazdadjoke.com/j/<joke_id', {
+        headers: {
+            'Accept':'application/json'
+        }
+    });
+    const joke= await jokeRes.json();
+    //set the new joke
+   jokeEle.innerHTML=joke.joke;
+}
